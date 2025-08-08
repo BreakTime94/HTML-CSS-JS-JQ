81416b0651592685cbea0a948dcc9031947c583b
BreakTime94 <kimchan9408@gmail.com>
2025-08-08 21:51:22 +0900

    Js 복습 1일차

---
+++ practice/Javascript/variable.js
@@ -0,0 +1,25 @@
+let sum = undefined;
+
+console.log(typeof sum)
+
+sum = null;
+
+console.log(typeof sum);
+
+let first = "holy"
+
+let last = "moly"
+
+console.log(`이런 젠장 ! ${first}${last}`)
+
+console.log(sum instanceof Object);
+
+console.log(2 ** 4)
+sum = '4'
+console.log(sum)
+console.log(typeof +sum)
+console.log(sum + 0)
+
+let c;
+
+console.log(c = 3)
