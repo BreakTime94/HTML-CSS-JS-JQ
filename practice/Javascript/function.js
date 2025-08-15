c9838d2d52801f1a44dfd48c659d877d305161f7
BreakTime94 <kimchan9408@gmail.com>
2025-08-15 23:56:39 +0900

    250815 16장 프로퍼티와 어트리뷰트  중간까지 공부

--- practice/Javascript/function.js
+++ practice/Javascript/function.js
@@ -60,3 +60,59 @@ function bar1 () {
 
 foo2();
 bar1();
+
+// ---- 15장 let과 const
+
+console.log("=======15장 let과 const ==========")
+
+// console.log(nani) 이 친구는 애초에 ReferenceError가 터짐
+
+//let과 const는 변수 hoisting이 안 일어나는 것처럼 보이지만 이뤄진다. 다만 temporal dead zone이 존재하여, 선언 이후 값이 초기화되기 전에 참조를 하려고 하면
+// 에러를 빵 터뜨린다!
+let nani;
+
+console.log(nani)
+
+nani = "나아니이";
+
+console.log(nani)
+
+console.log("===============16장 프로퍼티와 어트리뷰트==========")
+
+const obj1 = {name : "kim"}
+
+console.log(Object.getOwnPropertyDescriptor(obj1, "name"));
+
+obj1.age = 30;
+
+console.log(Object.getOwnPropertyDescriptors(obj1));
+
+const person = {
+  firstName : 'Chan',
+  LastName : 'Kim',
+
+  get fullName() {
+    return `${this.firstName} ${this.LastName}`;
+  },
+  set fullName(name) {
+    return [this.firstName, this.LastName] = name.split(" ");
+  }
+};
+
+console.log(person.firstName + ' ' + person.LastName);
+
+console.log(person.fullName);
+
+person.fullName = "야 인마"; // 프로퍼티는 함수 호출이 되지 않는다. 값을 저장하면 그 값에 따라서 호출이 된다
+
+console.log(person);
+
+console.log(person.fullName);
+
+console.log(Object.getOwnPropertyDescriptor(person, "firstName"));
+console.log(Object.getOwnPropertyDescriptor(person, "fullName"));
+
+
+
+
+
